dee97ea9ebd7064667badc92174e8dd27b4eaf6a
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-01-21 17:41:02 +0300

    feat: hero component

--- src/components/homeHero/Hero.jsx
+++ src/components/homeHero/Hero.jsx
@@ -4,17 +4,18 @@ import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <div className="hero-container justify-evenly gap-5 md:flex-row flex-col-reverse items-center flex p-2">
-      <div className="flex flex-col items-center">
-        <h1 className="font-serif text-2xl">Welcome to Mbao Zetu</h1>
-        <Link to="/shop"><button className="border mt-2 p-2 border-gray-200 shadow-lg rounded-lg font-serif w-40">
-          View Furniture
-        </button>
+    <div className="hero-container flex flex-col-reverse text-center lg:flex-row lg:justify-evenly items-center">
+      <div className="">
+        <h1 className="font-serif lg:text-2xl text-2xl">Welcome to Mbao Zetu</h1>
+        <Link to="/shop">
+          <button className="border hover:scale-105 transition-transform ease-in-out bg-[rgba(95,88,50,0.8)] text-white mt-2 p-2 border-gray-200 shadow-lg rounded-lg font-serif w-40">
+            View Furniture
+          </button>
         </Link>
       </div>
 
-      <div className="w-[400px] md:w-[550px]">
-        <img src={hero} alt="hero.jpg" />
+      <div className="lg:w-auto md:w-auto sm:w-auto">
+        <img src={hero} alt="hero.jpg" className='w-[600px]'/>
       </div>
     </div>
   );
