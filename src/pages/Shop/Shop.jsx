a920e2cf78b967fbacbd7f9e35b5a5dd05cd8172
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-01-25 21:36:43 +0300

    feat: shoppage

---
+++ src/pages/Shop/Shop.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Product from '../../components/product/Product';
+
+const Shop = () => {
+  return (
+    <div className="p-4 lg:mx-20">
+    <Product />
+  </div>
+  )
+}
+
+export default Shop;
