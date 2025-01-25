295843fb7848cad27b0a66628c30e488f50efe25
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-01-25 21:36:17 +0300

    feat: homepage

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Hero from '../../components/homeHero/Hero';
+
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default Home;
