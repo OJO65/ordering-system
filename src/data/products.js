827ef6b3cd1ebe451dda07235877a589e51c49b7
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-01-25 21:33:02 +0300

    feat: product data

---
+++ src/data/products.js
@@ -0,0 +1,75 @@
+import chair from '../assets/images/chair.jpg';
+import coffeeTable from '../assets/images/coffee table.jpg';
+import table from '../assets/images/office table.jpg';
+import doubleSofa from '../assets/images/double sofa.avif';
+import receptionChair from '../assets/images/reception chair.jpg';
+import reclinerSofa from '../assets/images/recliner sofa.jpg';
+import singleSofa from '../assets/images/single sofa.jpg';
+import schoolTable from '../assets/images/school table.jpg';
+import deskChair from '../assets/images/desk chair.jpg';
+
+export const products = [
+    {
+        id:1,
+        name: "Chair",
+        description: "This is a chair",
+        image: chair,
+        price:  5000
+    },
+    {
+        id:2,
+        name: "Coffee Table",
+        description: "This is a coffee table",
+        image: coffeeTable,
+        price:  55000
+    },
+    {
+        id:3,
+        name: "Table",
+        description: "This is a table",
+        image: table,
+        price:  15000
+    },
+    {
+        id:4,
+        name: "Double Sofa",
+        description: "This is a double sofa",
+        image: doubleSofa,
+        price:  65000
+    },
+    {
+        id:5,
+        name: "Reception Chair",
+        description: "This is a reception chair",
+        image: receptionChair,
+        price:  85000
+    },
+    {
+        id:6,
+        name: "Recliner Sofa",
+        description: "This is a recliner sofa",
+        image: reclinerSofa,
+        price: 105000
+    },
+    {
+        id:7,
+        name: "Single Sofa",
+        description: "This is a single sofa",
+        image: singleSofa,
+        price:  95000
+    },
+    {
+        id:8,
+        name:"School Table",
+        description: "This is a school table",
+        image: schoolTable,
+        price:  5000
+    },
+    {
+        id:9,
+        name:"Desk Chair",
+        description: "This is a desk chair",
+        image: deskChair,
+        price: 15000
+    },
+]
